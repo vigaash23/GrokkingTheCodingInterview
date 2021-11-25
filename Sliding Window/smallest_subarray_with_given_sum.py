f399cd0118517dac096f26cba5017d3565fbfb02
Vigaash Sivasothy <46359930+vigaash23@users.noreply.github.com>
2021-11-25 02:34:04 -0500

    Update smallest_subarray_with_given_sum.py

--- Sliding Window/smallest_subarray_with_given_sum.py
+++ Sliding Window/smallest_subarray_with_given_sum.py
@@ -1,3 +1,5 @@
+#Smallest Subarray with a given sum (easy)
+
 #Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
 
 def smallest_subarray_with_given_sum(s, arr):
